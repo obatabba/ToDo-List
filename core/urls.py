94edb5d8483f1e84f98064f2f5c6ad10974b11fc
Obada Tabbaa <obatabba@gmail.com>
2025-01-04 04:03:09 +0300

    Add user login/logout functionality.
    write views, urls, and templates and making changes for handling users logging in and logging out.

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,18 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib import messages
 from django.shortcuts import render
 from .models import Task
+
+
+class Login(LoginView):
+    template_name = 'login.html'
+
+
+class Logout(LogoutView):
+    next_page = 'home'
+    def dispatch(self, request, *args, **kwargs):
+        messages.success(request, "You have been logged out successfully.")
+        return super().dispatch(request, *args, **kwargs)
 
 
 def home(request):
