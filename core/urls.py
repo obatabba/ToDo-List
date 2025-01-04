80357866f10143ed1a4979c3c0f22a3b03c51ec6
Obada Tabbaa <obatabba@gmail.com>
2025-01-04 04:43:02 +0300

    Add user registration functionality.
    Write required view, url and template for handling users registraion. A few changes to the navbar and login template.

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Task
 
 
@@ -13,6 +15,18 @@ class Logout(LogoutView):
     def dispatch(self, request, *args, **kwargs):
         messages.success(request, "You have been logged out successfully.")
         return super().dispatch(request, *args, **kwargs)
+    
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
 
 def home(request):
