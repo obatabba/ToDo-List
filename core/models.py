03b2b3ec3625149e417d0f800d899f6087bcb4a0
Obada Tabbaa <obatabba@gmail.com>
2025-01-05 03:24:11 +0300

    Add status field to Task model.

--- core/models.py
+++ core/models.py
@@ -9,9 +9,16 @@ class Task(models.Model):
         'L': 'Low'
     }
 
+    STATUS_CHOICES = {
+        'I': 'Incomplete',
+        'C': 'Complete',
+        'O': 'Overdue'
+    }
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=80)
     description = models.TextField(null=True, blank=True)
     priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default='L')
     added_at = models.DateTimeField(auto_now=True)
     due_date = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='I')
